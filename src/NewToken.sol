c80cf7e1a0a0400b7496b1d2380486fb7aa3a56f
Theo Dale <theo@mockchain.xyz>
2025-09-08 18:07:24 +1000

    feat: add ERC20 NewToken.sol

---
+++ src/NewToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NewToken is ERC20, Ownable {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply_);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
