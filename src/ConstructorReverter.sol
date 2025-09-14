86a0ca4ea0eefb7b3ed0cf19b1c4f5664bc2c3df
Theo Dale <theo@mockchain.xyz>
2025-09-14 15:54:21 +1000

    asd

--- src/ConstructorReverter.sol
+++ src/ConstructorReverter.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.19;
 
 contract ConstructorReverter {
+    uint256 public x;
 
-        uint256 public x;
-        
     constructor() {
         revert("Constructor called");
     }
-
-
 
     function setX(uint256 _x) public {
         x = _x;
